63f430128b191d5bfc4f3af468b725751c65a801
arielva <szymon.wasiak96@gmail.com>
2018-12-18 00:17:07 +0100

    add packages, add contection to db in DAL folder and webconfig, remove not in use files

---
+++ Backend/Infrastructure/DAL/DBContext.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace Backend.Infrastructure.DAL
+{
+    public class DBContext : IDisposable
+    {
+        SqlConnection connection;
+
+        public DBContext()
+        {
+            string Server = WebConfigurationManager.AppSettings["Server"].ToString();
+            string Database = WebConfigurationManager.AppSettings["Database"].ToString();
+            string User = WebConfigurationManager.AppSettings["User"].ToString();
+            string Password = WebConfigurationManager.AppSettings["Password"].ToString();
+            string connectionString = String.Format("Server={0};uid={1};pwd={2};Database={3};Connect Timeout=20;MultipleActiveResultSets=true", Server, User, Password, Database);
+            this.connection = new SqlConnection(connectionString);
+        }
+
+        private void Open()
+        {
+            this.connection.Open();
+        }
+
+        public void Dispose()
+        {
+            if (this.connection != null)
+            {
+                this.connection.Dispose();
+                this.connection = null;
+            }
+        }
+
+        public SqlCommand CreateCommand(string storedProcedureName)
+        {
+            SqlCommand command = new SqlCommand(storedProcedureName);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+            command.Connection = this.connection;
+            return command;
+        }
+    }
+}
